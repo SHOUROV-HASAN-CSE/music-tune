7191456b22135970c70fd978e51781f90bfa9e98
Shouov Hasan <shouovhasan172@gmail.com>
2023-03-03 01:25:32 +0600

    header and footer created

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Shared/Footer/Footer';
+import Header from '../components/Shared/Header/Header';
 
 const Main = () => {
   return (
     <div>
-      
+      <Header/>
+      <Outlet/>
+      <Footer/>
     </div>
   );
 };
